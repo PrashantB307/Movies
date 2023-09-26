fced2ec84519594b853f9aa9d824878679716004
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-26 17:22:14 +0530

    :boom:  :boom:    Banner  :boom:  :boom:

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -12,7 +12,15 @@ export default class Banner extends Component {
             <span className="visually-hidden">Loading...</span>
           </div>
         ) : (
-          
+          <div className="card banner-card">
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              className="card-img-top banner-img"
+              alt="..."
+            />
+            <h5 className="card-title banner-title">{movie.original_title}</h5>
+            <p className="card-text banner-text">{movie.overview}</p>
+          </div>
         )}
       </>
     );
