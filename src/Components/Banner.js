a4570031095f99d5b473c2498212efde29211aee
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-26 17:21:08 +0530

    26-9-2023

---
+++ src/Components/Banner.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { movies } from "./getMovies";
+
+export default class Banner extends Component {
+  render() {
+    //console.log(movies.results[0]);
+    let movie = movies.results[0];
+    
+  }
+}
