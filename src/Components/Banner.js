dbc6c1b44da01a3fd91a006baa37fdf2e3db8891
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-26 17:21:38 +0530

    26-9-2023

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,6 +5,16 @@ export default class Banner extends Component {
   render() {
     //console.log(movies.results[0]);
     let movie = movies.results[0];
-    
+    return (
+      <>
+        {movie === "" ? (
+          <div className="spinner-border text-danger" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          
+        )}
+      </>
+    );
   }
 }
