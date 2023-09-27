a9f48de9afeb641842dc13787c4e6e9c844acaee
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-27 16:59:45 +0530

    27-9-2023

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,18 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "white",
+          color: "blue",
+          padding: "1rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        
+      </div>
     );
   }
 }
