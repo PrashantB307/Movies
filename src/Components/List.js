1d12ec4f32a12744ffe12f8f989fad333d61788c
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-28 20:18:56 +0530

    28-9-2023

---
+++ src/Components/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import axios from "axios";
+import API_KEY from "../secrets";
+
+export default class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hover: "",
+      parr: [1],
+      currPage: 1,
+      movies: [],
+      favMov: [], // this will store the id of the movies added to favoures
+    };
+  }
+
+  
+}
