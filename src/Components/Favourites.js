c30672e47da8e258e0f7b489110e4740808b94a8
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-30 20:58:14 +0530

    30-9-2023

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 export default class Favourites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      genre: [],
+      currGenre: "All Genre",
+      currText: "",
+      limit: 5,
+      currPage: 1,
+    };
+  }
+
   
 }
 
